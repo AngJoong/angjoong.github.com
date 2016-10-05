9311666a5834a32f38d60a14b1c7f4171ed20b5a
SeJoong <conquerorj@naver.com>
2016-10-05 09:56:17 +0900

    script

--- styles/index.js
+++ styles/index.js
@@ -11,32 +11,40 @@ var hide_lists = function (cb) {
     $('#fifth-btn').removeClass('disabled');
 };
 
-var show_first = function () {
-    $('#first-btn').removeClass('disabled');
-    $('#first').fadeOut(300, function () {
-      $('#second').fadeIn(300);
-      $('#third').fadeIn(300);
-      $('#fourth').fadeIn(300);
-      $('#fifth').fadeIn(300);
+var show_projects = function () {
+    $('#posts-btn').removeClass('disabled');
+    $('#posts').fadeOut(300, function () {
+        $('#projects').fadeIn(300)
     });
-    $('#second-btn').addClass('disabled');
-    $('#third-btn').addClass('disabled');
-    $('#fourth-btn').addClass('disabled');
-    $('#fifth-btn').addClass('disabled');
+    $('#projects-btn').addClass('disabled')
+};
+
+var show_first = function () {
+    $('#second-btn').removeClass('disabled');
+    $('#third-btn').removeClass('disabled');
+    $('#fourth-btn').removeClass('disabled');
+    $('#fifth-btn').removeClass('disabled');
+    $('#first').fadeIn(300, function () {
+      $('#second').fadeOut(300);
+      $('#third').fadeOut(300);
+      $('#fourth').fadeOut(300);
+      $('#fifth').fadeOut(300);
+    });
+    $('#first-btn').addClass('disabled');
 };
 
 var show_second = function () {
-    $('#second-btn').removeClass('disabled');
-    $('#second').fadeOut(function () {
-      $('#first').fadeIn(300);
-      $('#third').fadeIn(300);
-      $('#fourth').fadeIn(300);
-      $('#fifth').fadeIn(300);
+    $('#first-btn').removeClass('disabled');
+    $('#third-btn').removeClass('disabled');
+    $('#fourth-btn').removeClass('disabled');
+    $('#fifth-btn').removeClass('disabled');
+    $('#second').fadeIn(function () {
+      $('#first').fadeOut(300);
+      $('#third').fadeOut(300);
+      $('#fourth').fadeOut(300);
+      $('#fifth').fadeOut(300);
     });
-    $('#first-btn').addClass('disabled');
-    $('#third-btn').addClass('disabled');
-    $('#fourth-btn').addClass('disabled');
-    $('#fifth-btn').addClass('disabled');
+    $('#second-btn').addClass('disabled');
 };
 
 var show_third = function () {
