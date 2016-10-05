1cd05bbc8a41aafa4dcf6e545489cffb81652945
SeJoong <conquerorj@naver.com>
2016-10-05 09:42:26 +0900

    test

--- styles/index.js
+++ styles/index.js
@@ -1,1 +1,83 @@
-var hide_lists=function(cb){$('#posts').fadeOut(300);$('#projects').fadeOut(300);$('#posts-btn').removeClass('disabled');$('#projects-btn').removeClass('disabled')};var show_projects=function(){$('#posts-btn').removeClass('disabled');$('#posts').fadeOut(300,function(){$('#projects').fadeIn(300)});$('#projects-btn').addClass('disabled')};var show_posts=function(){$('#projects-btn').removeClass('disabled');$('#projects').fadeOut(function(){$('#posts').fadeIn(300)});$('#posts-btn').addClass('disabled')};
+var hide_lists = function (cb) {
+    $('#first').fadeOut(300);
+    $('#second').fadeOut(300);
+    $('#third').fadeOut(300);
+    $('#fourth').fadeOut(300);
+    $('#fifth').fadeOut(300);
+    $('#first-btn').removeClass('disabled');
+    $('#second-btn').removeClass('disabled');
+    $('#third-btn').removeClass('disabled');
+    $('#fourth-btn').removeClass('disabled');
+    $('#fifth-btn').removeClass('disabled');
+};
+
+var show_first = function () {
+    $('#first-btn').removeClass('disabled');
+    $('#first').fadeOut(300, function () {
+      $('#second').fadeIn(300);
+      $('#third').fadeIn(300);
+      $('#fourth').fadeIn(300);
+      $('#fifth').fadeIn(300);
+    });
+    $('#second-btn').addClass('disabled');
+    $('#third-btn').addClass('disabled');
+    $('#fourth-btn').addClass('disabled');
+    $('#fifth-btn').addClass('disabled');
+};
+
+var show_second = function () {
+    $('#second-btn').removeClass('disabled');
+    $('#second').fadeOut(function () {
+      $('#first').fadeIn(300);
+      $('#third').fadeIn(300);
+      $('#fourth').fadeIn(300);
+      $('#fifth').fadeIn(300);
+    });
+    $('#first-btn').addClass('disabled');
+    $('#third-btn').addClass('disabled');
+    $('#fourth-btn').addClass('disabled');
+    $('#fifth-btn').addClass('disabled');
+};
+
+var show_third = function () {
+    $('#third-btn').removeClass('disabled');
+    $('#third').fadeOut(function () {
+      $('#first').fadeIn(300);
+      $('#second').fadeIn(300);
+      $('#fourth').fadeIn(300);
+      $('#fifth').fadeIn(300);
+    });
+    $('#first-btn').addClass('disabled');
+    $('#second-btn').addClass('disabled');
+    $('#fourth-btn').addClass('disabled');
+    $('#fifth-btn').addClass('disabled');
+};
+
+var show_fourth = function () {
+    $('#fourth-btn').removeClass('disabled');
+    $('#fourth').fadeOut(function () {
+      $('#first').fadeIn(300);
+      $('#second').fadeIn(300);
+      $('#third').fadeIn(300);
+      $('#fifth').fadeIn(300);
+    });
+    $('#first-btn').addClass('disabled');
+    $('#second-btn').addClass('disabled');
+    $('#third-btn').addClass('disabled');
+    $('#fifth-btn').addClass('disabled');
+};
+
+
+var show_fifth = function () {
+    $('#fifth-btn').removeClass('disabled');
+    $('#fifth').fadeOut(function () {
+      $('#first').fadeIn(300);
+      $('#second').fadeIn(300);
+      $('#third').fadeIn(300);
+      $('#fourth').fadeIn(300);
+    });
+    $('#first-btn').addClass('disabled');
+    $('#second-btn').addClass('disabled');
+    $('#third-btn').addClass('disabled');
+    $('#fourth-btn').addClass('disabled');
+};
